4ea0d5151bcfc8f7d68a410825f711cc8011db85
Davi <44237144+daviseer@users.noreply.github.com>
2019-11-11 21:29:07 -0300

    Add files via upload

--- copyNewLockSreenImages.bat
+++ copyNewLockSreenImages.bat
@@ -1,8 +1,20 @@
 cd C:\Users\Davi\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
 
+IF NOT EXIST c:\Users\davi\New (
+
+
+mkdir C:\Users\Davi\New
 copy * C:\Users\Davi\New
 
-cd c:\Users\davi\New
+cd C:\Users\davi\New
 
 ren *.* *.jpg
 
+) ELSE (
+
+copy * C:\Users\Davi\New
+
+cd C:\Users\davi\New
+
+ren *.* *.jpg
+)
