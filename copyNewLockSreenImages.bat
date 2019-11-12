e3d556860fa720ac1f84ef41c80cdacbe8727b0d
Davi <44237144+daviseer@users.noreply.github.com>
2019-11-11 21:11:39 -0300

    Add files via upload
    Esta função copia as imagens que estão em seu lockscreen do Windows 10 (as imagens que aparecem na tela de login do windows). Substituir meu nome pelo seu nome de usuario antes de executar.

---
+++ copyNewLockSreenImages.bat
@@ -0,0 +1,8 @@
+cd C:\Users\Davi\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
+
+copy * C:\Users\Davi\New
+
+cd c:\Users\davi\New
+
+ren *.* *.jpg
+
