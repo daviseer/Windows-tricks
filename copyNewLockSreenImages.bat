968f599ad4544a860736a87ac0491b270bddcab1
Davi <44237144+daviseer@users.noreply.github.com>
2019-11-11 21:39:49 -0300

    Update copyNewLockSreenImages.bat

--- copyNewLockSreenImages.bat
+++ copyNewLockSreenImages.bat
@@ -1,20 +1,20 @@
-cd C:\Users\Davi\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
+cd C:\Users\%username%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
 
-IF NOT EXIST c:\Users\davi\New (
+IF NOT EXIST c:\Users\%username%\New (
 
 
-mkdir C:\Users\Davi\New
-copy * C:\Users\Davi\New
+mkdir C:\Users\%username%\New
+copy * C:\Users\%username%\New
 
-cd C:\Users\davi\New
+cd C:\Users\%username%\New
 
 ren *.* *.jpg
 
 ) ELSE (
 
-copy * C:\Users\Davi\New
+copy * C:\Users\%username%\New
 
-cd C:\Users\davi\New
+cd C:\Users\%username%\New
 
 ren *.* *.jpg
 )
